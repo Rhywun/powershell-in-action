ea845d9347d7dda58c29118ceb84e05331ddaf33
Patrick Huffer <Patrick.Huffer@amtrustfinancial.com>
2017-12-07 17:00:14 -0500

    Middle of ch06

--- ch06/ch06.ps1
+++ ch06/ch06.ps1
@@ -58,6 +58,70 @@ get-soup
 get-soup -please
 get-soup -please tomato
 
+# Get/Update/Set
+
+$characterData = @{
+    'Linus'  = @{ age = 8; human = $true}
+    'Lucy'   = @{ age = 8; human = $true}
+    'Snoopy' = @{ age = 2; human = $true}
+}
+
+function Get-Character ($name = '*') {
+    foreach ($entry in $characterData.GetEnumerator() | Write-Output) {
+        if ($entry.Key -like $name) {
+            $properties = @{ 'Name' = $entry.Key } +
+            $entry.Value
+            New-Object PSCustomObject -Property $properties
+        }
+    }
+}
+
+function Set-Character {
+    process {
+        $characterData[$_.name] =
+        @{
+            age   = $_.age
+            human = $_.human
+        }
+    }
+}
+
+function Update-Character (
+    [string] $name = '*',
+    [int] $age,
+    [bool] $human
+) {
+    begin {
+        if ($PSBoundParameters.'name') {
+            $name = $PSBoundParameters.name
+            [void] $PSBoundParameters.Remove('name')
+        }
+    }
+    process {
+        if ($_.name -like $name) {
+            foreach ($p in $PSBoundParameters.GetEnumerator()) {
+                $_.($p.Key) = $p.value
+            }
+        }
+        $_
+    }
+}
+
+#
+
+function numbers { 2+2; 9/3; [math]::Sqrt(27) }
+
+function numbers2 {
+    $i = 1
+    while ($i -le 10) {
+        $i
+        $i++
+    }
+}
+
+
+
+
 #
 
 function my-func ($x) {
