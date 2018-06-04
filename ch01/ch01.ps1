70577efa65715b11725848c5566ad6d63f8a95f3
Patrick Huffer <rhywun@rhywun.com>
2018-06-04 07:49:04 -0400

    Middle of ch01

--- ch01/ch01.ps1
+++ ch01/ch01.ps1
@@ -6,19 +6,35 @@ Get-ChildItem /private/var/log/*.log |
 
 ([xml] [System.Net.WebClient]::new().
     DownloadString('http://blogs.msdn.com/powershell/rss.aspx')).
-        RSS.Channel.Item |
-            Format-Table title, link
+RSS.Channel.Item | Format-Table title, link
 
 (2 + 2) * 3 / 7 | Out-File -FilePath ./foo.txt -Encoding utf8
 
-$a = Get-ChildItem /private/var/log/*.log |
-    Sort-Object -Property Length -Descending
-Write-Host $a[1]
-Write-Host ($a | Select-Object -First 1)
+# Note that you can't use `ls` or `sort` on MacOS
+Get-ChildItem | Sort-Object -Descending
+Get-ChildItem | Sort-Object Length
+Get-ChildItem | Sort-Object Length -Descending
+Get-ChildItem | Sort-Object Length -Descending | Select-Object -First 1
+Get-ChildItem | Sort-Object Length -Descending | Select-Object -First 1 -Prop Directory
 
 $total = 0
 Get-ChildItem | ForEach-Object { $total += $_.Length }
 Write-Host $total
-# Isn't there any way to simplify this?
 
-Get-Command | Where-Object ParameterSets.Count -GT 3 | Format-List Name
+# Isn't there any way to simplify this?
+# Well... there's this I guess:
+Get-ChildItem | ForEach-Object { $total = 0 } { $total += $_.Length } { $total }
+
+Get-Process | Sort-Object WorkingSet -Descending | Select-Object -First 3
+
+# Looping
+# all of these are equivalent
+
+$i = 0
+while ($i++ -lt 10) {if ($i % 2) {"$i is odd"}}
+
+foreach ($i in 1..10) {if ($i % 2) {"$i is odd"}}
+
+1..10 | ForEach-Object {if ($_ % 2) {"$i is odd"}}
+
+#
--- ch01/hello.ps1
+++ ch01/hello.ps1
@@ -1,3 +1,5 @@
+#!/usr/bin/env pwsh
+
 function hello {
     param($name = 'bub')
     Write-Output "Hello $name, how are you?"
